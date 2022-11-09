8d83f34ced0a8213d7f69df6eb91622a1c0e36eb
Dan <danilon37@gmail.com>
2022-11-08 21:50:09 -0300

    shiftJava

--- src/aula_02/DesvioIdade.java
+++ src/aula_02/DesvioIdade.java
@@ -1,9 +1,24 @@
 package aula_02;
 
+import java.util.Scanner;
+
 public class DesvioIdade {
 
 	public static void main(String[] args) {
 		int idade;
+		
+		System.out.println("Digite a idade:");
+        Scanner leitor = new Scanner(System.in);
+        idade = leitor.nextInt();
+        leitor.close();
+        
+        if (idade >= 16) {
+            
+        }else if (idade <= 18){
+        	
+        }else if (idade > 18){
+        	       	
+        }
 
 	}
 
