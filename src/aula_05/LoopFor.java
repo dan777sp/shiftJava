d7fdc810fa8dc0e501f2cb35a6d4561787c4f4e7
Dan <danilon37@gmail.com>
2022-11-16 21:23:00 -0300

    ShiftJava

--- src/aula_05/LoopFor.java
+++ src/aula_05/LoopFor.java
@@ -6,21 +6,44 @@ public class LoopFor {
 
 	public static void main(String[] args) {
 		// media de nota dos alunos
-		int qtdAlunos;
-		double nota = 0, media = 0;
+//		int qtdAlunos;
+//		double nota = 0, media = 0;
+//
+//		Scanner leitor = new Scanner(System.in);
+//		System.out.println("Digite a quantidade de alunos:");
+//		qtdAlunos = leitor.nextInt();
+//
+//		for (int i = 0; i < qtdAlunos; i++) {
+//			System.out.println("Digite a nota do aluno " + (i + 1) + ":");
+//			nota = leitor.nextDouble();
+//			media = media + nota;
+//		}
+//		leitor.close();
+//
+//		System.out.println("A media dos " + qtdAlunos + " alunos eh de: " + String.format("%.1f", (media / qtdAlunos)));
+		
+		// media de nota dos alunos pares e impares
+				int qtdAlunos;
+				double nota = 0, mediaPares = 0, mediaImpares = 0;
 
-		Scanner leitor = new Scanner(System.in);
-		System.out.println("Digite a quantidade de alunos:");
-		qtdAlunos = leitor.nextInt();
+				Scanner leitor = new Scanner(System.in);
+				System.out.println("Digite a quantidade de alunos:");
+				qtdAlunos = leitor.nextInt();
 
-		for (int i = 0; i < qtdAlunos; i++) {
-			System.out.println("Digite a nota do aluno " + (i + 1) + ":");
-			nota = leitor.nextDouble();
-			media = media + nota;
-		}
-		leitor.close();
+				for (int i = 0; i < qtdAlunos; i++) {
+					System.out.println("Digite a nota do aluno " + (i + 1) + ":");
+					nota = leitor.nextDouble();
+					if (i % 2 == 0) {
+						mediaPares = mediaPares + nota;
+					} else {
+						mediaImpares = mediaImpares + nota;
+					}
+						
+				}
+				leitor.close();
 
-		System.out.println("A media dos " + qtdAlunos + " alunos eh de: " + String.format("%.1f", (media / qtdAlunos)));
+				System.out.println("A media dos alunos pares eh de: " + String.format("%.1f", (mediaPares / qtdAlunos)));
+				System.out.println("A media dos alunos impares eh de: " + String.format("%.1f", (mediaImpares / qtdAlunos)));
 
 	}
 
